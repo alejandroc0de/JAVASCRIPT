55db7907664c91f218a36a5c8401183d298af114
Alejandro Carrillo <carrilloalejandro0215@gmail.com>
2025-09-20 13:03:00 -0500

    ADDED FUNCTIONALITY
    Agregamos los day left count, agregamos un calendario para poder escoger la fecha due, mejoramos como se ve el top bar y ahora cuando se borra un event no vuelve a aparecer cuando hacemos reload

--- myIdeas/to-doList/index.js
+++ myIdeas/to-doList/index.js
@@ -1,5 +1,5 @@
 window.onload = loadTasks;
-
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 function getTask(){
     task = document.getElementById("taskInput");
@@ -20,32 +20,58 @@ function addToList(task){
 
     let statusSelected = clone.querySelector(".selectStatus")
 
+    let dateChoosen = document.getElementById("dueDate");
+    const daysLeft = getDaysLeft(dateChoosen.value)
+    console.log(daysLeft)
+    clone.querySelector(".dueIn").textContent = `${daysLeft} Days`
+
     tarjeta = clone.querySelector(".displayTask")
     tarjeta.classList.add(selectedPriority)
     document.querySelector(".displayTasks").append(clone)
 
     let taskId = "task-"+Date.now()
     tarjeta.setAttribute("id",taskId)
+    console.log(taskId)
+
 
     let taskInput = document.getElementById("taskInput")
     taskInput.value = "" 
 
 
     taskInput.placeholder ="Enter a Task"
-    saveTask(task,selectedPriority)
+    saveTask(task,selectedPriority,daysLeft,taskId)
 
 
     statusSelected.addEventListener("change", (event) => {
         let card = document.getElementById(taskId)
         if(statusSelected.value == "Completed"){
             card.remove()
+            tasks = tasks.filter(task => task.id!== taskId)
+            localStorage.setItem("tasks",JSON.stringify(tasks))
         }
     })
 }
 
-function saveTask(task,priority){
-    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-    tasks.push({text: task, priority: priority, status: "Not Started"});
+function getDaysLeft(dateChoosen){
+
+    let dateNow = new Date()
+    day = dateNow.getDate()
+    month = dateNow.getMonth()
+    year = dateNow.getFullYear()
+
+    const dateDue = new Date()
+    dateChoosen = dateChoosen.split("-")
+    console.log(dateChoosen)
+    dateDue.setDate(dateChoosen[2])
+    dateDue.setMonth(dateChoosen[1]-1)
+    dateDue.setFullYear(dateChoosen[0])
+    let tiempoRestante = (dateDue-dateNow)
+    tiempoRestante = tiempoRestante / 1000 / 60 / 60 / 24
+    return tiempoRestante
+}
+
+function saveTask(task,priority,daysLeft,taskId){
+    tasks.push({text: task, priority: priority, status: "Not Started",daysLeft:daysLeft, id : taskId});
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
@@ -59,6 +85,8 @@ function loadTasks(){
         clone.querySelector(".taskText").textContent = element.text
         clone.querySelector(".taskPriority").textContent = element.priority
 
+        clone.querySelector(".dueIn").textContent = `${element.daysLeft} Days`
+
         let statusSelected = clone.querySelector(".selectStatus")
         statusSelected.value = element.status;
 
@@ -66,11 +94,12 @@ function loadTasks(){
         tarjeta.classList.add(element.priority)
         document.querySelector(".displayTasks").append(clone)
 
-        let taskId = "task-"+Date.now()
-        tarjeta.setAttribute("id",taskId)
+        tarjeta.setAttribute("id",element.id)
 
         let taskInput = document.getElementById("taskInput")
         taskInput.value = "" 
+
+        console.log(localStorage)
 
 
         statusSelected.addEventListener("change", (event) => {
@@ -82,3 +111,8 @@ function loadTasks(){
         
     });
 }
+
+flatpickr("#dueDate", {
+  dateFormat: "Y-m-d",
+  defaultDate: "today"
+});
